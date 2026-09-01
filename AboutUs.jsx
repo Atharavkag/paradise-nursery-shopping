f593cf8aad41c432b30c1a5c7e77ba68da01bc0e
Atharav Kagde <atharavkagde7@gmail.com>
2026-09-02 00:47:37 +0530

    update

---
+++ AboutUs.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AboutUs() {
+  return (
+    <div className="about-us">
+      <h1>About Paradise Nursery</h1>
+      <p>Welcome to Paradise Nursery, your number one source for all things plants. We're dedicated to giving you the very best of greenery, with a focus on quality, customer service, and uniqueness.</p>
+    </div>
+  );
+}
+
+export default AboutUs;
